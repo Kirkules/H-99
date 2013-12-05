0d8fc4a2ee71c17a7f1d0ac8de19affc669fecc7
Kirk Boyer <kirk.boyer@gmail.com>
2013-12-05 14:59:33 -0700

    insert-added question 7

--- H99_7.hs
+++ H99_7.hs
@@ -1,6 +1,10 @@
 -- Kirk Boyer
 -- 12/3/2013
 -- Haskell 99 Questions
--- Question 7: Flatten a nested list structure.
+-- Question 7: Flatten a nested list structure. Using NestedList definition from problem statement on haskell.org.
+data NestedList a = Elem a | List [NestedList a] deriving (Show)
 
-
+flatten :: NestedList a -> [a]
+flatten (List []) = []
+flatten (Elem a) = [a]
+flatten (List (x:xs)) = (flatten x)++(flatten (List xs))
